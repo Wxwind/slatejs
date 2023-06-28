14c794afb5e9c64bb1c777c4277c164618945aa7
wxwind <373903632@qq.com>
2023-06-28 16:18:13 +0800

    fix: rollup and dts settings

--- packages/slate/rollup.config.js
+++ packages/slate/rollup.config.js
@@ -2,10 +2,14 @@ import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 export default {
   input: 'src/index.ts',
-  plugins: [dts()],
+  plugins: [typescript()],
   output: [
     {
-      file: 'dist/index.d.ts',
+      file: 'dist/cjs/index.js',
+      format: 'cjs',
+    },
+    {
+      file: 'dist/es/index.js',
       format: 'es',
     },
   ],
