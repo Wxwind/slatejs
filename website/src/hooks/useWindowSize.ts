3427f70d76c6127dc67c30b09048efb9776acaf3
wxwind <373903632@qq.com>
2023-11-28 19:40:48 +0800

    fix: eslint not work

--- website/src/hooks/useWindowSize.ts
+++ website/src/hooks/useWindowSize.ts
@@ -1,17 +1,17 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 export const useWindowSize = () => {
   const [width, setWidth] = useState<number>(0);
   const [height, setHeight] = useState<number>(0);
 
-  const handleWindowResize = () => {
+  const handleWindowResize = useCallback(() => {
     setWidth(window.innerWidth);
     setHeight(window.innerHeight);
-  };
+  }, []);
 
   useEffect(() => {
     handleWindowResize();
-  }, []);
+  }, [handleWindowResize]);
 
   useEffect(() => {
     window.addEventListener('resize', handleWindowResize);
