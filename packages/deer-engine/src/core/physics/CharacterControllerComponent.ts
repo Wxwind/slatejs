32ce097b7fb800599b2fb6216b8350b11907d244
wxwind <373903632@qq.com>
2024-12-03 18:33:20 +0800

    temp saved

--- packages/deer-engine/src/core/physics/CharacterControllerComponent.ts
+++ packages/deer-engine/src/core/physics/CharacterControllerComponent.ts
@@ -4,12 +4,68 @@ import { RigidbodyComponent } from './RigidbodyComponent';
 import { ICharacterController } from './interface';
 import { objectToLocalPosition } from '@/util';
 import { Vector3 } from 'three';
+import { PhysicsControllerNonWalkableModeEnum } from './enum';
 
 export class CharacterControllerComponent extends RigidbodyComponent {
   public type = 'CharacterControllerComponent' as const;
 
   _nativeRigidbody: ICharacterController;
   private _tempPosition = new Vector3();
+
+  private _radius: number = 1;
+  public get radius(): number {
+    return this._radius;
+  }
+  public set radius(v: number) {
+    this._radius = v;
+    this._nativeRigidbody.setRadius(v);
+  }
+
+  private _height: number = 2;
+  public get height(): number {
+    return this._height;
+  }
+  public set height(v: number) {
+    this._height = v;
+    this._nativeRigidbody.setHeight(v);
+  }
+
+  private _slopeLimit: number = 0.707;
+  public get slopeLimit(): number {
+    return this._slopeLimit;
+  }
+  public set slopeLimit(v: number) {
+    this._slopeLimit = v;
+    this._nativeRigidbody.setSlopeLimit(v);
+  }
+
+  private _stepOffset: number = 0.5;
+  public get stepOffset(): number {
+    return this._stepOffset;
+  }
+  public set stepOffset(v: number) {
+    this._stepOffset = v;
+    this._nativeRigidbody.setStepOffset(v);
+  }
+
+  private _upDirection: IVector3 = { x: 0, y: 1, z: 0 };
+  public get upDirection(): IVector3 {
+    return this._upDirection;
+  }
+  public set upDirection(v: IVector3) {
+    this._upDirection = v;
+    this._nativeRigidbody.setUpDirection(v);
+  }
+
+  private _nonWalkableMode: PhysicsControllerNonWalkableModeEnum =
+    PhysicsControllerNonWalkableModeEnum.PREVENT_CLIMBING;
+  public get nonWalkableMode(): PhysicsControllerNonWalkableModeEnum {
+    return this._nonWalkableMode;
+  }
+  public set nonWalkableMode(v: PhysicsControllerNonWalkableModeEnum) {
+    this._nonWalkableMode = v;
+    this._nativeRigidbody.setNonWalkableMode(v);
+  }
 
   constructor(entity: Entity) {
     super(entity);
