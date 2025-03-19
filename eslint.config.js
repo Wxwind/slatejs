564a178ac6c07c18296da837ccbf14f116b82fed
wxwind <373903632@qq.com>
2025-03-19 15:33:45 +0800

    chore: flat eslint config

---
+++ eslint.config.js
@@ -0,0 +1,61 @@
+import pluginPrettierConfig from 'eslint-plugin-prettier/recommended';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import tseslint from 'typescript-eslint';
+import eslint from '@eslint/js';
+import globals from 'globals';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  pluginPrettierConfig,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2022,
+      },
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    ignores: ['dist', 'eslint.config.js', 'node_modules'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    rules: {
+      '@typescript-eslint/no-empty-interface': 'warn',
+      'no-empty-pattern': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+    },
+  },
+  {
+    files: ['website/**/*.{js,mjs,cjs,jsx,ts,tsx}'],
+    ...reactPlugin.configs.flat.recommended,
+    ...reactPlugin.configs.flat['jsx-runtime'],
+    languageOptions: {
+      ...reactPlugin.configs.flat.recommended.languageOptions,
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+  {
+    files: ['website/**/*.{js,mjs,cjs,jsx,ts,tsx}'],
+    plugins: { 'react-hooks': reactHooks },
+    rules: {
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  }
+);
